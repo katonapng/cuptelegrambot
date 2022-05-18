f47e5acd4a2980c4e37eb16682b1e9f97b7994ca
kaustika <snufkinson@yandex.ru>
2022-05-18 13:49:52 +0300

    add randomized pic choice

--- disk.py
+++ disk.py
@@ -1,14 +1,23 @@
 import yadisk
+import random
 from bottoken import yadisk_token
 
 
 def get_pictures(total_cup_num: int, black_in: bool):
-    """bla bla.
-    Returns:
-        list: urls to pics
+    """
+
+    :param total_cup_num:
+    :param black_in:
+    :return: list of download-links
     """
     # TODO ALL
+    # randomize what cups to send
     y = yadisk.YaDisk(token=yadisk_token)
-    files = [f.file for f in y.listdir("/cupbot/A")]
-    print(files)
-    return files
+    files_A = [f.file for f in y.listdir("/cupbot/A")]
+    if not black_in:
+        chosen_files = random.sample(files_A, k=total_cup_num)
+    else:
+        files_black = [f.file for f in y.listdir("/cupbot/black")]
+        chosen_files = random.sample(files_A, k=total_cup_num - 1)
+        chosen_files.append(random.sample(files_black, k=1))
+    return chosen_files
